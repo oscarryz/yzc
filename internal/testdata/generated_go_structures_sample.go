7e8a0679f9e5f91658f1aed9dc2aea17251ef7b9
Oscar Reyes <oscarryz@gmail.com>
2024-11-02 08:51:05 -0500

    Adds example of generated code

---
+++ internal/testdata/generated_go_structures_sample.go
@@ -0,0 +1,83 @@
+package main
+
+import "fmt"
+
+/*
+// filename is e.yz
+s : "The sound of the ocean calm my soul"
+
+g : {
+	t String
+}
+
+f : {
+	s : "The sound of the ocean calm my soul"
+	g : {
+		t String
+		println(t)
+		t.reverse()
+	}
+	h : {
+		s.reverse()
+	}
+}
+
+print("Hello `s`, `g`, `f`, `f.s`, `f.g`, `f.g.t`")
+s = f.g("This is fun")
+*/
+type (
+	_e struct {
+		s  string
+		g  *_e_g
+		_f *_e_f
+	}
+	_e_f struct {
+		s string
+		g *_e_f_g
+		h *_e_f_h
+	}
+	_e_f_g struct {
+		t string
+	}
+	_e_f_h struct {
+		__f *_e_f
+		results []any
+	}
+	_e_g struct {
+		t string
+	}
+)
+
+func (gfg *_e_f_g) apply() {
+	fmt.Printf("%s\n", gfg.t)
+	gfg.t = std_string_reverse(gfg.t)
+}
+func (efh *_e_f_h) apply() {
+	std_string_reverse(efh.__f.s)
+}
+
+func std_string_reverse(s string) string {
+	return s
+}
+func (f *_e_f) apply() {
+}
+func (e *_e) apply() {
+	e._f = &_e_f{
+		s: "The sound of the ocean calm my soul",
+		g: &_e_f_g{},
+		h: &_e_f_h{},
+	}
+	e._f.h.__f = e._f
+
+
+	fmt.Printf("Hello %s, %v, %v, %s, %v, %v", e.s, e.g, e._f, e._f.s, e._f.g, e._f.g.t)
+	e._f.g.t = "This is fun"
+	e._f.g.apply()
+	e.s = e._f.g.t
+
+}
+func main() {
+	e := _e{}
+	e.apply()
+}
+
--- issues.md
+++ issues.md
@@ -1,3 +1,8 @@
+## Create an example of the generated Go code
+
+Complete [generated_go_structures_sample.go](internal/testdata/generated_go_structures_sample.go) to include asynchronous calls. 
+
+Include a recursive method call.
 ## Pass build options to the compiler
 
 We can keep the generated source files or discard them. 
