cc041dce94816b8f964984ed4694578b597866fb
Oscar Reyes <oscarryz@gmail.com>
2024-11-03 05:29:26 -0600

    Adds "structured concurrency" on generated sample
    Is not exactly structured concurrency, but explores how to
    make a boc wait until all the internal bocs are completed while
    executing then concurrenly.
    
    It stills need more work as every inner boc will have to do the same but
    proves that is possible to run each boc concurrently and still wait
    until the whole block finishes.
    
    I need to figure out how to wait if a value is expected
    
    `a:func()` vs `func()`

--- internal/testdata/generated_go_structures_sample.go
+++ internal/testdata/generated_go_structures_sample.go
@@ -1,31 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 /*
 // filename is e.yz
 s : "The sound of the ocean calm my soul"
 
-g : {
-	t String
-}
-
-f : {
-	s : "The sound of the ocean calm my soul"
 	g : {
 		t String
-		println(t)
-		t.reverse()
 	}
-	h : {
-		s.reverse()
+
+	f : {
+		s : "The sound of the ocean calm my soul"
+		g : {
+			t String
+			println(t)
+			t.reverse()
+		}
+		h : {
+			s.reverse()
+		}
 	}
-}
 
 print("Hello `s`, `g`, `f`, `f.s`, `f.g`, `f.g.t`")
 s = f.g("This is fun")
 */
 type (
+	// conc helps to run functions concurrently
+	// and waits until they complete
+	// similar idea as https://github.com/sourcegraph/conc but simplified for this sample
+	conc struct {
+		wg sync.WaitGroup
+	}
+	runner interface {
+		run()
+	}
+	// transformed structs
 	_e struct {
 		s  string
 		g  *_e_g
@@ -40,28 +53,39 @@ type (
 		t string
 	}
 	_e_f_h struct {
-		__f *_e_f
+		__f     *_e_f
 		results []any
 	}
 	_e_g struct {
 		t string
 	}
+	_factorial struct {
+		n      int
+		result int
+	}
 )
 
-func (gfg *_e_f_g) apply() {
+func (c *conc) Run(r runner) {
+	c.wg.Add(1)
+	go func() {
+		defer c.wg.Done()
+		r.run()
+	}()
+}
+func (gfg *_e_f_g) run() {
 	fmt.Printf("%s\n", gfg.t)
 	gfg.t = std_string_reverse(gfg.t)
 }
-func (efh *_e_f_h) apply() {
+func (efh *_e_f_h) run() {
 	std_string_reverse(efh.__f.s)
 }
 
 func std_string_reverse(s string) string {
 	return s
 }
-func (f *_e_f) apply() {
+func (f *_e_f) run() {
 }
-func (e *_e) apply() {
+func (e *_e) run() {
 	e._f = &_e_f{
 		s: "The sound of the ocean calm my soul",
 		g: &_e_f_g{},
@@ -69,45 +93,42 @@ func (e *_e) apply() {
 	}
 	e._f.h.__f = e._f
 
-
-	fmt.Printf("Hello %s, %v, %v, %s, %v, %v", e.s, e.g, e._f, e._f.s, e._f.g, e._f.g.t)
+	fmt.Printf("Hello %s, %v, %v, %s, %v, %v\n", e.s, e.g, e._f, e._f.s, e._f.g, e._f.g.t)
 	e._f.g.t = "This is fun"
-	e._f.g.apply()
+	e._f.g.run()
 	e.s = e._f.g.t
-
-}
-func main() {
-	e := _e{}
-	e.apply()
-	f:= &_factorial{}
-	f.n = 4
-	f.apply()
-	fmt.Println(f.result)
 
 }
 
 /*
 factorial #(n Int, Int)
 factoria = { n Int
-	n == 0 ? { 1 }
-	{ n * factorial(n -1 )}
-}
+
+		n == 0 ? { 1 }
+		{ n * factorial(n -1 )}
+	}
+
 factorial(2)
 */
-type (
-	_factorial struct { 
-		n int
-		result int
-	}
-)
-func (f *_factorial) apply() {
-	if f.n == 0 { 
+func (f *_factorial) run() {
+	if f.n == 0 {
 		f.result = 1
-		return 
+		return
 	} else {
 		n := f.n
 		f.n = f.n - 1
-		f.apply()
+		f.run()
 		f.result = n * f.result
 	}
+}
+func main() {
+	var c conc
+	e := &_e{}
+	c.Run(e)
+	f := &_factorial{}
+	f.n = 4
+	c.Run(f)
+	c.wg.Wait()
+	fmt.Println(f.result)
+
 }
